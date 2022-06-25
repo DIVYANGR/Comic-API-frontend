f0daf9449de425fa79c30e073236413dc51875dd
Divyang <52504061+DIVYANGR@users.noreply.github.com>
2022-06-25 02:23:33 -0400

    Update config.js
    changed host URL

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 
 // dev
-const config = {
-    baseURL: "http://127.0.0.1:8080/"
-}
+// const config = {
+//     baseURL: "http://127.0.0.1:8080/"
+// }
 
 
 
 // prod
 
-// const config = {
-//     baseURL: "https://comic-xkcd-dc.herokuapp.com/"
-// }
+const config = {
+    baseURL: "https://secret-gorge-09341.herokuapp.com/"
+}
 
 
 
